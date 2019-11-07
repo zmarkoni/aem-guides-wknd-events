9c7e7be8fd067871a41bc5779e89ccda9c4ef7e6
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-11-07 10:11:13 +0100

    create TEXT component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,16 @@
+// Start by initializing the App with the JSON Model from AEM.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ModelManager, Constants } from '@adobe/cq-spa-page-model-manager';
 import App from './App';
+// include all React components that map to an AEM component
+import "./components/MappedComponents";
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function render(model) {
+    ReactDOM.render(( <App cqChildren={ model[Constants.CHILDREN_PROP] } cqItems={ model[Constants.ITEMS_PROP] } cqItemsOrder={ model[Constants.ITEMS_ORDER_PROP] }
+            cqPath={ ModelManager.rootPath } locationPathname={ window.location.pathname }/>),  document.getElementById('root'));
+}
+
+ModelManager.initialize({ path: process.env.REACT_APP_PAGE_MODEL_PATH }).then(render);
